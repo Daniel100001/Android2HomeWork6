3b7d0c6e3800a60b49620e79dc5ee5f2d0f9f369
Daniel100001 <danldplayer2gmail.com>
2023-04-15 00:13:13 +0600

    It's Android 2 Home Work 2

--- app/src/main/java/com/example/android2homework2/ui/onBoard/OnBoardFragment.kt
+++ app/src/main/java/com/example/android2homework2/ui/onBoard/OnBoardFragment.kt
@@ -1,14 +1,19 @@
 package com.example.android2homework2.ui.onBoard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableLayout
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
 import com.example.android2homework2.R
 import com.example.android2homework2.databinding.FragmentOnBoardBinding
 import com.example.android2homework2.ui.adapters.OnBoardAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class OnBoardFragment : Fragment() {
 
@@ -20,25 +25,42 @@ class OnBoardFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentOnBoardBinding.inflate(inflater,container,false)
+        binding = FragmentOnBoardBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        text= view.findViewById(R.id.next_txt)
-        intilialize()
+        text = view.findViewById(R.id.next_txt)
+        initialization()
         clickButton()
     }
 
 
-    private fun intilialize() {
+    private fun initialization() {
         binding.viewPager.adapter = OnBoardAdapter(this@OnBoardFragment)
+        TabLayoutMediator(binding.tableLayout, binding.viewPager) { tab, pos ->
+            when (pos) {
+                0 -> {
+                    tab.setIcon(R.drawable.graypoint)
+
+                }
+                1 -> {
+                    tab.setIcon(R.drawable.graypoint)
+                }
+                2 -> {
+                    tab.setIcon(R.drawable.graypoint)
+                }
+
+
+            }
+        }.attach()
     }
+
     private fun clickButton() = with(binding.viewPager) {
-        binding.nextTxt.setOnClickListener(){
-            if (currentItem < 2){
-                setCurrentItem(currentItem +1,true)
+        binding.nextTxt.setOnClickListener() {
+            if (currentItem < 2) {
+                setCurrentItem(currentItem + 1, true)
             }
         }
     }
--- app/src/main/java/com/example/android2homework2/ui/onBoard/OnBoardPagingFragment.kt
+++ app/src/main/java/com/example/android2homework2/ui/onBoard/OnBoardPagingFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.airbnb.lottie.LottieDrawable
+import com.example.android2homework2.R
 import com.example.android2homework2.databinding.FragmentOnBoardPagingBinding
 
 
@@ -28,21 +31,39 @@ class OnBoardPagingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initialization()
+        goToHomeFragment()
     }
+
+
 
     private fun initialization() = with(binding) {
         when (requireArguments().getInt(ARG_POSITION)) {
             0 -> {
-                onTxt.text = "First Fragment"
+                onTxt.text = "Очень удобный функционал"
+                animationView.repeatCount = LottieDrawable.INFINITE
+                animationView.repeatMode = LottieDrawable.REVERSE
+                animationView.setAnimation(R.raw.anim1)
             }
             1 -> {
-                onTxt.text= "Second Fragment"
+                onTxt.text= "Быстрый, качественный продукт"
+                animationView.repeatCount = LottieDrawable.INFINITE
+                animationView.repeatMode = LottieDrawable.REVERSE
+                animationView.setAnimation(R.raw.anim)
 
             }
             2 ->{
-                onTxt.text = "Third Fragment"
+                onTxt.text = "Куча функций и интересных фишек"
+                animationView.repeatCount = LottieDrawable.INFINITE
+                animationView.repeatMode = LottieDrawable.REVERSE
+                animationView.setAnimation(R.raw.anim2)
+                fragmentOnBoardPagingText.text = "Начать работу"
 
             }
         }
+    }
+    private fun goToHomeFragment() {
+        binding.fragmentOnBoardPagingText.setOnClickListener(View.OnClickListener {
+            findNavController().navigate(R.id.action_onBoardPagingPFragment_to_homeFragment)
+        })
     }
 }
